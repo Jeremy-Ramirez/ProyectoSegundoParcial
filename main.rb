d0244cd10f3262f31d5ecbf2474e732e994ea626
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2022-01-16 17:49:25 -0500

    Jeremy-Ramirez-Scraping

---
+++ main.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+require 'nokogiri'
+require 'csv'
+
+
+CSV.open('cursos.csv', 'wb') do |csv|
+  conf=0;
+  csv << %w[TítuloCurso Precio Duración Autor]
+  puts "Scrapeando la url"
+    
+    link="https://www.reed.co.uk/courses/?keywords=programming"
+    udemyHTML= URI.open(link)
+    datos=udemyHTML.read
+    parsed_content = Nokogiri::HTML(datos)
+    
+    inf_container = parsed_content.css('.course-list')
+    inf_container.css('li').each do |curso|
+      titulo=curso.css('h2.course-title>a.block-link-xs').inner_text[0..-1]
+      precio=curso.css('div.course-price>span.price').inner_text
+      duracion=curso.css('div.course-features li.clock-icon').inner_text[1..10]
+      autor=''
+      autorImg= curso.css('div.course-features img.img-thumbnail').each do |img|
+        autor=img['alt']
+        #print(autor)
+      end     
+      if(titulo!="" && precio !="" && duracion!="")
+        csv << [titulo.to_s,precio.to_s,duracion.to_s,autor.to_s]
+      end
+    end
+end
